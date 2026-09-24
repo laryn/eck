9112e329d074ea6e3ad81992df384b48b1720f44
laryn <laryn@kragtbakker.com>
2026-09-24 17:15:42 -0400

    Issue #11: Convert entity type and bundle to CMI.

--- eck.api.php
+++ eck.api.php
@@ -7,30 +7,30 @@
 /**
  * Respond to the creation of a new ECK entity type.
  *
- * @param EntityType $entity_type
+ * @param EckEntityType $entity_type
  *   The entity type is being created.
  */
-function hook_eck_entity_type_insert(EntityType $entity_type) {
+function hook_eck_entity_type_insert(EckEntityType $entity_type) {
 
 }
 
 /**
  * Respond to the updating of a new ECK entity type.
  *
- * @param EntityType $entity_type
+ * @param EckEntityType $entity_type
  *   The entity type is being update.
  */
-function hook_eck_entity_type_update(EntityType $entity_type) {
+function hook_eck_entity_type_update(EckEntityType $entity_type) {
 
 }
 
 /**
  * Respond to the deletion of a new ECK entity type.
  *
- * @param EntityType $entity_type
+ * @param EckEntityType $entity_type
  *   The entity type is being deleted.
  */
-function hook_eck_entity_type_delete(EntityType $entity_type) {
+function hook_eck_entity_type_delete(EckEntityType $entity_type) {
 
 }
 
